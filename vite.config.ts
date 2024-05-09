32b04aabe1bf258f26220119109bc11a3d7b7007
TheComputerM <mudit.somani00@gmail.com>
2024-05-10 03:32:35 +0530

    chore: deploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tsconfigPaths({ root: "./" }), solid()],
+  base: "/dart-ast-explorer/",
   build: {
     target: 'esnext'
   }
