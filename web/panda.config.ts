b062f6f27aa22d95659275c595594f97caa55ef2
TheComputerM <mudit.somani00@gmail.com>
2025-01-16 05:28:12 +0530

    chore: update web packages

--- web/panda.config.ts
+++ web/panda.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from "@pandacss/dev";
+import { createPreset } from '@park-ui/panda-preset'
+import blue from '@park-ui/panda-preset/colors/blue'
+import slate from '@park-ui/panda-preset/colors/slate'
 
 export default defineConfig({
 	preflight: true,
-	presets: ["@pandacss/preset-base", "@park-ui/panda-preset"],
+	presets: [createPreset({ accentColor: blue, grayColor: slate, radius: 'sm' })],
 	include: ["./src/**/*.{js,jsx,ts,tsx}"],
 	exclude: [],
 	jsxFramework: "solid",
